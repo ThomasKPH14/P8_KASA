5a260ff1dc6de499cd3d105a818cc1acf7515bb6
ThomasKPH14 <Kropfitsch14@Outlook.fr>
2023-07-31 15:55:21 +0200

    Fin du projet

--- kasa/src/pages/Error404.jsx
+++ kasa/src/pages/Error404.jsx
@@ -5,7 +5,7 @@ function Error404() {
   <div className='main-page'>
     <div className='error-container'>
       <h1 className='error-container__title'>404</h1>
-      <p className='error-container__text'>Oups! La page que <br></br>vous demandez n'existe pas.</p>
+      <p className='error-container__text'>Oups! La page que vous demandez n'existe pas.</p>
       <a href="/" className='error-container__url'>Retourner sur la page d’accueil</a>
     </div>
   </div>
